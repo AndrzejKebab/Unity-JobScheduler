1b691eed0ffe55f201989704e9aed5a144a34890
Andrzej Kebab <grzesiugora123@wp.pl>
2025-04-05 22:59:08 +0200

    Use IJobData Schedule method XD forgot about that

--- JobForScheduler.cs
+++ JobForScheduler.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Cysharp.Threading.Tasks;
-using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 
@@ -10,7 +9,6 @@ namespace PatataGames.JobScheduler
 	///     Data structure for IJobFor implementations to be scheduled.
 	/// </summary>
 	/// <typeparam name="T">The job type, which must be a struct implementing IJobFor.</typeparam>
-	[BurstCompile]
 	public struct JobForData<T> : IJobData where T : struct, IJobFor
 	{
 		/// <summary>
@@ -22,6 +20,8 @@ namespace PatataGames.JobScheduler
 		///     The length of the array to process.
 		/// </summary>
 		public int       ArrayLength;
+
+		public int InnerBatchSize;
 		
 		/// <summary>
 		///     Optional job handle that must complete before this job can start.
@@ -34,7 +34,12 @@ namespace PatataGames.JobScheduler
 		/// <returns>A JobHandle that can be used to track the job's completion.</returns>
 		public JobHandle Schedule()
 		{
-			return Job.Schedule(ArrayLength, Dependency);
+			return Job.ScheduleByRef(ArrayLength, Dependency);
+		}
+
+		public JobHandle ScheduleParallel()
+		{
+			return Job.ScheduleParallelByRef(ArrayLength, InnerBatchSize, Dependency);
 		}
 	}
 	
@@ -43,12 +48,11 @@ namespace PatataGames.JobScheduler
 	///     Provides batched scheduling and completion of jobs with yielding to prevent main thread blocking.
 	/// </summary>
 	/// <typeparam name="T">The job type, which must be an unmanaged struct implementing IJobFor.</typeparam>
-	[BurstCompile]
 	public struct JobForScheduler<T>  : IJobScheduler, IDisposable 
 		where T : unmanaged, IJobFor
 	{
 		private JobSchedulerBase       baseScheduler;
-		private NativeList<JobForData<T>> jobList;
+		private NativeQueue<JobForData<T>> jobQueue;
 
 		/// <summary>
 		///     Initializes a new instance of the JobForScheduler struct.
@@ -58,12 +62,12 @@ namespace PatataGames.JobScheduler
 		public JobForScheduler(int capacity = 64, byte batchSize = 32)
 		{
 			baseScheduler = new JobSchedulerBase(capacity, batchSize);
-			jobList = new NativeList<JobForData<T>>(capacity, Allocator.Persistent);
+			jobQueue = new NativeQueue<JobForData<T>>(Allocator.Persistent);
 		}
 
 		/// <summary>
 		///     Controls how many jobs are processed before yielding back to the main thread.
-		///     Default is 8.
+		///     Default is 32.
 		/// </summary>
 		public byte BatchSize
 		{
@@ -80,7 +84,7 @@ namespace PatataGames.JobScheduler
 		/// <summary>
 		///     Returns the number of jobs in the queue waiting to be scheduled.
 		/// </summary>
-		public int JobsToSchedule => jobList.Length;
+		public int JobsToSchedule => jobQueue.Count;
 		
 		/// <summary>
 		///     Returns the total number of jobs being managed by the scheduler.
@@ -102,10 +106,9 @@ namespace PatataGames.JobScheduler
 		/// <param name="job">The job to add.</param>
 		/// <param name="arrayLength">The length of the array to process.</param>
 		/// <param name="dependency">Optional job handle that must complete before this job can start.</param>
-		[BurstCompile]
 		public void AddJob(T job, int arrayLength, JobHandle dependency = default)
 		{
-			jobList.Add(new JobForData<T>
+			jobQueue.Enqueue(new JobForData<T>
 			             {
 				             Job         = job,
 				             ArrayLength = arrayLength,
@@ -117,7 +120,6 @@ namespace PatataGames.JobScheduler
 		///     Adds an external job handle to the tracking list.
 		/// </summary>
 		/// <param name="handle">The job handle to track.</param>
-		[BurstCompile]
 		public void AddJobHandle(JobHandle handle)
 		{
 			baseScheduler.AddJobHandle(handle);
@@ -128,15 +130,16 @@ namespace PatataGames.JobScheduler
 		///     blocking the main thread for too long.
 		/// </summary>
 		/// <returns>A UniTask that completes when all jobs are scheduled.</returns>
-		[BurstCompile]
 		public async UniTask ScheduleJobsAsync()
 		{
 			byte count = 0;
 
-			foreach (JobForData<T> data in jobList)
+			// Use indexing instead of foreach to allow struct modification
+			for (var i = 0; i < jobQueue.Count; i++)
 			{
 				count++;
-				JobHandle handle = data.Job.Schedule(data.ArrayLength, data.Dependency);
+				// Schedule the job and store the handle
+				JobHandle handle = jobQueue.Dequeue().Schedule();
 				baseScheduler.AddJobHandle(handle);
 
 				if (count < BatchSize) continue;
@@ -144,9 +147,7 @@ namespace PatataGames.JobScheduler
 				count = 0;
 			}
 
-			jobList.Clear();
-
-			if (count > 0) await UniTask.Yield();
+			jobQueue.Clear();
 		}
 
 		/// <summary>
@@ -154,7 +155,6 @@ namespace PatataGames.JobScheduler
 		///     blocking the main thread for too long.
 		/// </summary>
 		/// <returns>A UniTask that completes when all jobs are finished.</returns>
-		[BurstCompile]
 		public UniTask CompleteAsync()
 		{
 			return baseScheduler.CompleteAsync();
@@ -164,7 +164,6 @@ namespace PatataGames.JobScheduler
 		///     Completes all tracked jobs without yielding.
 		///     Use this when immediate completion is required.
 		/// </summary>
-		[BurstCompile]
 		public void CompleteImmediate()
 		{
 			baseScheduler.CompleteImmediate();
@@ -176,7 +175,7 @@ namespace PatataGames.JobScheduler
 		public void Dispose()
 		{
 			baseScheduler.Dispose();
-			jobList.Dispose();
+			jobQueue.Dispose();
 		}
 	}
 }
